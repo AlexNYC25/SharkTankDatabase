9b0c80ccdde7b7440fbe21f081f7beb2c1b38027
Alexis Montes <montes.alexis25@gmail.com>
2020-08-25 23:59:32 -0400

    created initial investor insert

---
+++ investors.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO investors ("investor_type", "investor_status", "investor_first_name", "investor_last_name")
+    VALUES ('Main', 'Active', 'Kevin', 'O''Leary');
+INSERT INTO investors ("investor_type", "investor_status", "investor_first_name", "investor_last_name")
+    VALUES ('Main', 'Active', 'Robert', 'Herjavec');
+INSERT INTO investors ("investor_type", "investor_status", "investor_first_name", "investor_middle_name", "investor_last_name")
+    VALUES ('Main', 'Active', 'Daymond', 'Garfield' ,'John');
+INSERT INTO investors ("investor_type", "investor_status", "investor_first_name", "investor_middle_name" ,"investor_last_name")
+    VALUES ('Main', 'Active', 'Barbara', 'Ann', 'Corcoran');
+INSERT INTO investors ("investor_type", "investor_status", "investor_first_name", "investor_last_name")
+    VALUES ('Main', 'Active', 'Kevin', 'Harrington');
